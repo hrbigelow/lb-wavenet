d88f7f0c7bba0ee3f21d3d6af460cb5fd04611d8
Henry Bigelow <hrbigelow@gmail.com>
2018-06-24 12:58:23 -0700

    data_funcs is working

--- data_funcs.py
+++ data_funcs.py
@@ -14,125 +14,101 @@
 
 import librosa
 import tensorflow as tf
+import numpy as np
 
-def parse_sample_map(sam_file):
+def gen_sample_map(sam_file):
     '''load a sample file with the format:
     voice_id /path/to/file1.wav
     voice_id /path/to/file2.wav
     ...
+    generate tuples (voice_id, wav_path)
     '''
-    sam_fh = open(sam_file)
-    sample_map = [(int(t[0]), t[1]) for t in 
-            [s.strip().split('\t') for s in 
-                sam_fh.readlines()]]
-    sam_fh.close()
-    return sample_map 
+    with open(sam_file) as sam_fh:
+        for s in sam_fh.readlines():
+            (vid, wav_path) = s.strip().split('\t')
+            yield (int(vid), wav_path)
+    return
 
 
-def read_wav_file(wav, sample_rate):
+def read_wav_file(wav_file, sample_rate):
     '''read the contents of a wav file, returning np.ndarray'''
-    audio, _ = librosa.load(wav, sr=sample_rate, mono=True)
-    audio = audio.reshape(-1, 1)
+    audio, _ = librosa.load(wav_file, sr=sample_rate, mono=True)
+    #audio = audio.reshape(-1, 1)
     return audio
 
 
-def concat_slices(itr, slice_sz, sess):
-    '''generates slices from a virtually concatenated set of .wav files.
-    itr produces { 'voice_id': int64 [], 'wav': int64 [None] } 
+def gen_wav_files(path_itr, sample_rate, sess):
+    '''consume an iterator that yields [voice_id, wav_path].
+    load the .wav file contents into a vector and return a tuple
+    '''
+    ne = path_itr.get_next()
+    while True:
+        try:
+            (vid, wav_path) = sess.run(ne)
+        except tf.errors.OutOfRangeError:
+            break
+        wav = read_wav_file(wav_path, sample_rate)
+        yield (int(vid), wav)
+    return
 
-    yields { 'offsets': int64 [None,2], 'raw': int64 [slice_sz] }
+
+def gen_concat_slices(wav_itr, slice_sz, sess):
+    '''generates slices from a virtually concatenated set of .wav files.
+    consume itr, which yields [voice_id, wav_data]
+
+    concatenate slices of slice_sz, yielding 
+    (int32 [None,2], float32 [slice_sz])
     where:
-    out['offsets'][i] = [voice_id, wav_offset, slice_offset]
-    out['raw'][t] = wav_val
+    out[0][i] = [voice_id, wav_offset, slice_offset]
+    out[1][t] = wav_val
     '''
     need_sz = slice_sz 
     offsets = []
-    spliced = tf.convert_to_tensor([], dtype=tf.int64)
-    next_elem = itr.get_next()
+    spliced = np.empty([0], np.float)
+    ne = wav_itr.get_next()
 
     while True:
         try:
-            wav = sess.run(next_elem)
+            (vid, wav) = sess.run(ne)
         except tf.errors.OutOfRangeError:
             break
-        raw = wav['raw']
-        vid = wav['vid']
+        wav_sz = wav.shape[0] 
         cur_item_pos = 0
         
-        while need_sz <= (item_sz - cur_item_pos):
-            spliced = tf.concat([spliced, raw[cur_item_pos:cur_item_pos + need_sz]], 0)
-            offsets += [vid, cur_item_pos, slice_sz - need_sz]
+        while need_sz <= (wav_sz - cur_item_pos):
+            spliced = np.append(spliced, wav[cur_item_pos:cur_item_pos + need_sz])
+            offsets.append([vid, cur_item_pos, slice_sz - need_sz])
             cur_item_pos += need_sz 
-            yield { 'offsets': tf.convert_to_tensor(offsets), 'spliced': spliced } 
+            yield (offsets, spliced) 
             offsets = [] 
-            spliced = tf.convert_to_tensor([], dtype=tf.int64)
+            spliced = np.empty([0], np.float) 
             need_sz = slice_sz 
-        if cur_item_pos != item_sz:
-            spliced = tf.concat([spliced, raw[cur_item_pos:]], 0) 
-            offsets += [vid, cur_item_pos, sliced_sz - need_sz] 
-            need_sz -= item_sz - cur_item_pos
+        if cur_item_pos != wav_sz:
+            splice = np.append(spliced, wav[cur_item_pos:])
+            offsets.append([vid, cur_item_pos, slice_sz - need_sz]) 
+            need_sz -= wav_sz - cur_item_pos
     return
 
 
-def gen_wav_files(path_itr, sample_rate, sess):
-    next_el = path_itr.get_next()
-    while True:
-        try:
-            path = sess.run(next_el)
-        except tf.errors.OutOfRangeError:
-            break
-        wav = read_wav_file(path['wav'], sample_rate)
-        yield (path['voice_id'], wav)
-    return
 
-
-def wav_concat_slices(sam_path, slice_sz, sample_rate, sess):
+def wav_dataset(sam_path, slice_sz, sample_rate, sess):
     '''parse a sample file and create a ts.data.Dataset of concatenated,
     labeled slices from it'''
-    samples = parse_sample_map(sam_path)
-    d1 = tf.data.Dataset.from_tensor_slices({ 
-        'voice_id': [i[0] for i in samples],
-        'wav': [i[1] for i in samples]
-        })
+    zero_d = tf.TensorShape([])
+    one_d = tf.TensorShape([None])
+    two_d = tf.TensorShape([None, 3])
+    d1 = tf.data.Dataset.from_generator(lambda: gen_sample_map(sam_path),
+            (tf.int32, tf.string),
+            (zero_d, zero_d))
+
     d2 = d1 # do shuffling and repetition here
-    d3 = tf.data.Dataset.from_generator(gen_wav_files,
-            (tf.int64, tf.int64), (tf.TensorShape([]), tf.TensorShape([None])),
-            (d2, sample_rate, sess))
-    out_types = (tf.int64, tf.int64)
-    out_shapes = (tf.TensorShape([None]), tf.TensorShape([None]))
-    d4 = tf.data.Dataset.from_generator(concat_slices, out_types, out_shapes,
-            (d3, slice_sz, sess))
+    i2 = d2.make_one_shot_iterator()
+
+    d3 = tf.data.Dataset.from_generator(lambda: gen_wav_files(i2, sample_rate, sess),
+            (tf.int32, tf.float32), (zero_d, one_d))
+    i3 = d3.make_one_shot_iterator()
+
+    d4 = tf.data.Dataset.from_generator(lambda: gen_concat_slices(i3, slice_sz, sess),
+            (tf.int32, tf.float32), (two_d, one_d))
     return d4
 
-
-
-def gen1(lim):
-    for i in range(lim):
-        yield i
-    return
-
-
-def gen2(itr, sess):
-    next_el = itr.get_next()
-    while True:
-        try:
-            v = sess.run(next_el)  
-        except tf.errors.OutOfRangeError:
-            break
-        #vi = sess.run(v)
-        #vt = tf.convert_to_tensor(vi)
-        yield v 
-    return
-
-
-
-ds1 = tf.data.Dataset.from_generator(lambda : gen1(10), (tf.int32), (tf.TensorShape([])))
-itr1 = ds1.make_one_shot_iterator()
-
-sess = tf.Session()
-ds2 = tf.data.Dataset.from_generator(lambda : gen2(itr1, sess), (tf.int32), (tf.TensorShape([])))
-itr2 = ds2.make_one_shot_iterator()
-
-
-
-        
