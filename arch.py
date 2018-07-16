8b1293e9072aa1e354589f7b7c5a1375558f79f7
Henry Bigelow <hrbigelow@gmail.com>
2018-07-16 11:54:09 -0700

    progress on imodel

--- arch.py
+++ arch.py
@@ -1,22 +1,18 @@
 import tensorflow as tf
-from enum import Enum
+from enum import IntEnum
 
 
-
-class ArchCat(Enum):
-    SIGNAL = 's'
-    GATE = 'g'
-    SKIP = 'k'
-    RESIDUAL = 'r'
-    PRE = 'x'
-    POST = 'y'
-
-
-def _make_key(channel_code, arch_cat, block=None, layer=None):
-    if block is None and layer is None:
-        return '{}{}'.format(arch_cat.value, channel_code)
-    else:
-        return '{}{}_{}_{}'.format(arch_cat.value, channel_code, block, layer)
+class ArchCat(IntEnum):
+    PRE = 1 
+    RESIDUAL = 2 
+    SKIP = 3
+    SIGNAL = 4 
+    GATE = 5 
+    GC_SIGNAL = 6
+    GC_GATE = 7 
+    GC_EMBED = 8
+    POST1 = 9
+    POST2 = 10
 
 
 class WaveNetArch(object):
@@ -49,83 +45,27 @@ class WaveNetArch(object):
         self.vars = {} 
         self.var_init_func = tf.contrib.layers.xavier_initializer_conv2d()
 
-
-    def create_vars(self):
-        '''create all trainable model parameters.
-        All variables except for the global conditioning ones are matrices
-        with dimensions corresponding to a number of channels of a certain type.
-        The following letter codes correspond with channels:
-
-            Q: quantization channels
-            R: residual channels
-            D: dilation channels
-            S: skip channels
-            P: post channels
-            G: global condition categories
-            E: global condition embeddings
-
-        This class stores references to all variables created in the graph under self.vars.
-        The variables are given default (meaningless) names that are not scoped in the tf.Graph.
-        Since this class handles the saving and restoring of variables, and graphs built from
-        these variables access them through self.vars, there is no need for a second naming
-        scheme.  When viewing the resulting graph in TensorBoard, it is best to detach them
-        from the main graph.
-            
-            '''
-        shape = {
-                'QR': [1, self.n_quant, self.n_res],
-                'DR': [1, self.n_dil, self.n_res],
-                'DS': [1, self.n_dil, self.n_skip],
-                'RD': [2, self.n_res, self.n_dil],
-                'SP': [1, self.n_skip, self.n_post],
-                'PQ': [1, self.n_post, self.n_quant],
-                'GE': [self.n_gc_category, self.n_gc_embed],
-                'ED': [1, self.n_gc_embed, self.n_dil]
+        self.shape = {
+                ArchCat.PRE: [1, self.n_quant, self.n_res],
+                ArchCat.RESIDUAL: [1, self.n_dil, self.n_res],
+                ArchCat.SKIP: [1, self.n_dil, self.n_skip],
+                ArchCat.SIGNAL: [2, self.n_res, self.n_dil],
+                ArchCat.GATE: [2, self.n_res, self.n_dil],
+                ArchCat.GC_SIGNAL: [2, self.n_gc_embed, self.n_dil],
+                ArchCat.GC_GATE: [2, self.n_gc_embed, self.n_dil],
+                ArchCat.GC_EMBED: [self.n_gc_category, self.n_gc_embed],
+                ArchCat.POST1: [1, self.n_skip, self.n_post],
+                ArchCat.POST2: [1, self.n_post, self.n_quant]
                 }
 
+    def get_variable(self, arch):
+        '''wrapper for tf.get_variable that supplies the proper shape and name
+        according to arch'''
+        return tf.get_variable(arch.name, self.shape[arch])
 
-        def _make_var(channel_code, arch_cat, block=None, layer=None):
-            var = tf.Variable(self.var_init_func(shape=shape[channel_code]))
-            self.vars[_make_key(channel_code, arch_cat, block, layer)] = var
-
-        _make_var('QR', ArchCat.PRE)
-        _make_var('SP', ArchCat.POST)
-        _make_var('PQ', ArchCat.POST)
-
-        if self.use_gc:
-            _make_var('GE', ArchCat.PRE)
+    # Create the initializer
+    self.var_init_op = tf.variables_initializer(list(self.vars.values()))
         
-        # create all layer-based variables
-        def _make_var_grid(channel_code, arch_cat):
-            for b in range(self.n_blocks):
-                for l in range(self.n_block_layers):
-                    _make_var(channel_code, arch_cat, b, l)
-
-        codes = ['RD', 'RD', 'DR', 'DS']
-        archs = [ArchCat.SIGNAL, ArchCat.GATE, ArchCat.RESIDUAL, ArchCat.SKIP]
-        if self.use_gc:
-            codes += ['ED', 'ED']
-            archs += [ArchCat.SIGNAL, ArchCat.GATE]
-
-        for (code, arch) in zip(codes, archs): 
-            _make_var_grid(code, arch)
-
-        # Create the initializer
-        self.var_init_op = tf.variables_initializer(list(self.vars.values()))
-        
-
-    def get_var(self, channel_code, arch_cat, block=None, layer=None):
-        '''retrieve a variable from the model.
-        Args:
-        channel_code: QR, GE, RD, ED, DR, DS, SP, PQ
-        arch_cat: ArchCat 
-        block and layer are ignored for QR, GE, SP and PQ
-        '''
-        key = _make_key(channel_code, arch_cat, block, layer)
-        item = self.vars[key]
-        return item
-
-
 
 
 
--- imodel.py
+++ imodel.py
@@ -28,8 +28,20 @@ class WaveNetGen(ar.WaveNetArch):
                 n_gc_category)
 
         self.chunk_sz = chunk_sz
+        # create all lookback buffers
+        self.input = tf.Variable(tf.zeros([None, self.chunk_sz + 1]),
+                name='lb_in',
+                trainable=False)
+        self.lookback = [tf.Variable(tf.zeros([None, 2**l + self.chunk_sz]),
+            name='lb_b{}_l{}'.format(b, l),
+            trainable=False)
+            for b in range(self.n_blocks)
+            for l in range(self.n_block_layers)]
+
 
     def _preprocess(self, prev_val):
+        '''everything done to the raw signal before it is
+        ready for the layers.'''  
         with tf.name_scope('preprocess'):
             if self.use_gc:
                 gc_tab = self.get_var('GE', ar.ArchCat.PRE)
@@ -37,40 +49,56 @@ class WaveNetGen(ar.WaveNetArch):
             pre_op = tf.matmul(prev_val, filt[0]) + tf.matmul(prev_val, filt[1])
         return pre_op
 
-    def _dilated_conv(self, prev_op, dilation, pos, block, layer):
+
+    def _dilated_conv(self, src_buf, dilation, pos):
         '''pos: zero-based position in the cached windoow'''
-        v = {}
-        gc = {}
-        cache = self.get_cache(block, layer)
         embed = self.get_embed()
-        for arch = [ar.ArchCat.SIGNAL, ar.ArchCat.GATE]:
-            filt = self.get_var('RD', arch, block, layer)
-            with tf.name_scope(arch.name):
-                v[arch] = tf.matmul(cache[pos], filt[0]) + tf.matmul(prev_op, filt[1])
-                if self.use_gc:
-                    gc_filt = self.get_var('ED', arch, block, layer)
-                    gc[arch] = tf.matmul(embed, gc_filt[0]) + tf.matmul(embed, gc_filt[1])
-           
-        (signal, gate) = (v[ar.ArchCat.SIGNAL], v[ar.ArchCat.GATE])
+
+        v = {}
+        for arch in [ar.ArchCat.SIGNAL, ar.ArchCat.GATE]:
+                filt = self.get_variable(arch)
+                v[arch] = tf.matmul(src_buf[pos], filt[0])
+                + tf.matmul(src_buf[pos + dilation], filt[1])
+
         if self.use_gc:
-            signal = tf.add(signal, gc[ar.ArchCat.SIGNAL], 'add_gc_signal')
-            gate = tf.add(gate, gc[ar.ArchCat.GATE], 'add_gc_gate')
+            for arch in [ar.ArchCat.GC_SIGNAL, ar.ArchCat.GC_GATE]: 
+                gc_filt = self.get_variable(arch)
+                gc_proj = tf.matmul(embed, gc_filt[0]) + tf.matmul(embed, gc_filt[1])
+                v[arch] = tf.add(v[arch], gc_proj, 'add')
 
-        post_signal = tf.tanh(signal)
-        post_gate = tf.sigmoid(gate)
-
-        z = post_signal * post_gate
-
+        z = tf.tanh(v[ar.ArchCat.SIGNAL]) * tf.sigmoid(v[ar.ArchCat.GATE])
         return z
 
-    def _chan_reduce(self, prev_op, block, layer):
-        sig_filt = self.get_var('DR', ar.ArchCat.RESIDUAL, block, layer)
-        skip_filt = self.get_var('DS', ar.ArchCat.SKIP, block, layer)
-        next_cache = self.get_next_cache(block, layer)
+
+    def _chan_reduce(self, prev_op):
+        '''simply provide the channel reducing operations'''
+        sig_filt = self.get_variable(ar.ArchCat.RESIDUAL)
+        skp_filt = self.get_variable(ar.ArchCat.SKIP)
+
         with tf.name_scope('signal'):
-            signal = tf.matmul(prev_op, sig_filt[0]) + tf.matmul(prev_op, sig_filt[1])
+            signal = tf.matmul(prev_op, sig_filt)
         with tf.name_scope('skip'):
-            skip = tf.matmul(prev_op, skip_filt[0]) + tf.matmul(prev_op, skip_filt[1])
+            skip = tf.matmul(prev_op, skip_filt)
         return signal, skip 
 
+    def _loop_cond(self, wpos, cont):
+        return cont 
+
+
+    def _loop_body(self, wpos, cont):
+        for b in range(self.n_blocks):
+            for bl in range(self.n_block_layers):
+                l = b * self.n_block_layers + bl
+                dil = 2**bl
+                with tf.variable_scope('dconv{}'.format(l)):
+                    dconv = self._dilated_conv(cur_buf, dil, wpos)
+
+        dst_buf = tf.get_variable('lookback_{}'.format(layer),
+                [None, dilation + self.chunk_sz, self.n_res])
+        scope = 'dconv{}'.format(layer)
+        op = tf.assign(dst_buf[pos], z)
+        with tf.control_dependencies([op]):
+            pass
+        with tf.variable_scope(scope, reuse=None):
+            pass
 
